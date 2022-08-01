31bfd5ff25dc430cfb8e67d230536db70ce5c01c
Анастасия Лютая <liutyivova72@gmail.com>
2022-08-01 18:16:58 +0300

    hw3 done

---
+++ js/script.js
@@ -0,0 +1,199 @@
+// Задание-1
+let Number = 4;
+console.log(Number)
+
+let String ="String";
+console.log(String)
+
+let Boolean = true;
+console.log(Boolean)
+
+let Undefined;
+console.log(Undefined)
+
+let Nul = null;
+console.log(Nul)
+
+let symbol = Symbol("id");
+console.log(symbol)
+
+let Bigint = (8);
+console.log(Bigint)
+
+let Object = {
+    name: "Vasa",
+    lastName: "Pup",
+    age:87
+};
+console.log(Object)
+
+//Задание-2
+let Variable="hyper";
+console.log(Variable)
+
+letVariable=5
+console.log(letVariable)
+
+varVariable="hyper";
+console.log(varVariable)
+
+varVariable=2
+console.log(varVariable)
+
+//Задание3
+parseFloat(String);
+console.log(String)
+
+parseInt(String);
+console.log(String)
+
+
+//Задание4
+let strPlusNumber = "str + number";
+console.log(Number)
+
+let strPlusString = "str + string";
+console.log(String)
+
+let strPlusBollen = "str + boolean";
+console.log(Boolean)
+
+let strPlusUndefined = "str + undefined";
+console.log(Undefined)
+
+let  strPlusNull = "str + null";
+console.log(Nul)
+
+let  strPlusSymbol = "str + symbol";
+console.log(Symbol(123))
+
+let  strPlusBigint = "str + bigint";
+console.log(Bigint)
+
+let  strObject = "str + object";
+console.log(Object)
+
+let NamPlusNam = "nam + nam";
+console.log(Number)
+
+let  NamPlusString = "nam + string";
+console.log(String)
+
+let NamPlusBoolean = "nam + boolean";
+console.log(Boolean)
+
+let  NamPlusUndefined = "nam + undefined";
+console.log(Undefined)
+
+let  NamPlusNull = "nam + null";
+console.log(Nul)
+
+let  NamPlusSymbol = "nam + symbol";
+console.log(Symbol(123))
+
+let  NamPlusBigint = "nam + bigint";
+console.log(Bigint)
+
+let  NamPlusObject = "nam + object";
+console.log(Object)
+
+//Задание-5
+let StrMinusNam = "str - nam";
+console.log(Number)
+
+let  StrMinusString = "str - string";
+console.log(String)
+
+let StrMinusBoolean = "str - boolean";
+console.log(Boolean)
+
+let StrMinusUndefined = "str - undefined";
+console.log(Undefined)
+
+let StrMinusNull = "str - null";
+console.log(Nul)
+
+let StrMinusSymbol = "str - symbol";
+console.log(Symbol(123))
+
+let StrMinusBigint = "str - bigint";
+console.log(Bigint)
+
+let StrMinusObject = "str - object";
+console.log(Object)
+
+let NamMinusNam = "nam - nam";
+console.log(Number)
+
+let NamMinusString = "nam - string";
+console.log(String)
+
+let NamMinusBoolean = "nam - boolean";
+console.log(Boolean)
+
+let NamMinusUndefined = "nam - undefined";
+console.log(Undefined)
+
+let NamMinusNull = "nam - null";
+console.log(Nul)
+
+let NamMinusSymbol = "nam - symbol";
+console.log(Symbol(123))
+
+let NamMinusBigint = "nam - bigint";
+console.log(Bigint)
+
+let NamMinusObject = "nam - Object";
+console.log(Object)
+
+//задание-6
+const login = prompt("Enter Login");
+const password = prompt("Enter password");
+if (login === "kykold" && password === "1234asdQQ"){alert("Добро пожаловать")}
+else {alert("Ошибка входа")}
+
+//Задание-7
+let minAge=16;
+let age=+prompt("Enter your age");
+if (age>=minAge){alert("Ошибка входа")}
+else (alert("Вход воспрещён"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
